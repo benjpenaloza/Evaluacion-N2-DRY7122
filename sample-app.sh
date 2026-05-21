0b482ac350a41fa381b828be1b379020d66aa524
benjpenaloza <benj.penaloza@duocuc.cl>
2026-05-21 07:40:31 +0000

    Fix tempdir and Docker image

--- sample-app.sh
+++ sample-app.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf tempdir
+
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -14,9 +16,14 @@ echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 9999" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+echo 'CMD ["python","/home/myapp/sample_app.py"]' >> tempdir/Dockerfile
 
 cd tempdir
+
+docker rm -f samplerunning || true
+
 docker build -t sampleapp .
+
 docker run -t -d -p 9999:9999 --name samplerunning sampleapp
+
 docker ps -a
